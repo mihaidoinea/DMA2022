c0a357f02da0fc7fa59605e296b1dd6b56ff0e6d
Mihai Doinea <mihai.doinea@oracle.com>
2022-10-12 11:19:34 +0300

    G1084_S03

--- s1084s03/src/main/java/com/example/s1084s03/MainActivity.java
+++ s1084s03/src/main/java/com/example/s1084s03/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.s1084s03;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void btnClick(View view)
+    {
+        Intent intent = new Intent(this,SecondActivity.class);
+        intent.putExtra("param1", "Hello from MainActivity!");
+        startActivity(intent);
     }
 }
---
+++ s1084s03/src/main/java/com/example/s1084s03/SecondActivity.java
@@ -0,0 +1,19 @@
+package com.example.s1084s03;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        Intent intent = getIntent();
+        String param1 = intent.getStringExtra("param1");
+        Toast.makeText(this,"Value: " + param1, Toast.LENGTH_LONG).show();
+    }
+}
