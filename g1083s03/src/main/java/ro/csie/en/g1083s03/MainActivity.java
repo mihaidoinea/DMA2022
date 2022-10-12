aa0aeeffbaff762fc70675ba4088d055d294e8d9
Mihai Doinea <mihai.doinea@oracle.com>
2022-10-12 18:14:50 +0300

    G1083_S03

--- g1083s03/src/main/java/ro/csie/en/g1083s03/MainActivity.java
+++ g1083s03/src/main/java/ro/csie/en/g1083s03/MainActivity.java
@@ -7,6 +7,7 @@ import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
@@ -50,6 +51,9 @@ public class MainActivity extends AppCompatActivity {
         //start normal activity mode
         //startActivity(intent);
         //start activity for result mode
-        activityResultLauncher.launch(intent);
+        //activityResultLauncher.launch(intent);
+
+        Intent callIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://acs.ase.ro"));
+        startActivity(callIntent);
     }
 }
