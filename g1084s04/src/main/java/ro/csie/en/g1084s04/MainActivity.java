11c425f6a6992c32b322e34fa6238fd28878ee0b
Mihai Doinea <mihai.doinea@oracle.com>
2022-10-19 12:09:01 +0300

    G1084S04

--- g1084s04/src/main/java/ro/csie/en/g1084s04/MainActivity.java
+++ g1084s04/src/main/java/ro/csie/en/g1084s04/MainActivity.java
@@ -3,12 +3,45 @@ package ro.csie.en.g1084s04;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Button btnSave;
+
+    class MyOnClickListener implements View.OnClickListener
+    {
+
+        @Override
+        public void onClick(View view) {
+
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getLifecycle().addObserver(new MyObservable());
+        btnSave = findViewById(R.id.btnSave);
+        btnSave.setOnClickListener(this);
+        btnSave.setOnClickListener(new MyOnClickListener());
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"Hello from btnSave",
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+        btnSave.setOnClickListener(view ->
+                Toast.makeText(getApplicationContext(),"Hello from btnSave",
+                        Toast.LENGTH_LONG).show());
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
