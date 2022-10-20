a7f78db164d55bf46cc6e6db46fdd5729ddbd5df
Mihai Doinea <mihai.doinea@oracle.com>
2022-10-20 09:20:23 +0300

    G1083_S04

--- g1083s04/src/main/java/ro/csie/en/g1083s04/MainActivity.java
+++ g1083s04/src/main/java/ro/csie/en/g1083s04/MainActivity.java
@@ -19,7 +19,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
