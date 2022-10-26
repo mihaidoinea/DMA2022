d9c5d5b912da2242190fc262cae0e25fab110950
Mihai Doinea <mihai.doinea@oracle.com>
2022-10-26 12:55:24 +0300

    G1083_S05

--- g1083s04/src/main/java/ro/csie/en/g1083s04/MainActivity.java
+++ g1083s04/src/main/java/ro/csie/en/g1083s04/MainActivity.java
@@ -2,13 +2,20 @@ package ro.csie.en.g1083s04;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.Spinner;
 import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -45,6 +52,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         });
         btnSave.setOnClickListener(view ->
         {
+            movie.setTitle(etTitle.getText().toString());
             Toast.makeText(getApplicationContext(),"Movie: " + movie, Toast.LENGTH_LONG).show();
         });
     }
@@ -53,8 +61,58 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnSave = findViewById(R.id.btnSave);
         etTitle = findViewById(R.id.etTitle);
         spGenre = findViewById(R.id.spGenre);
+        spGenre.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                String genre = adapterView.getItemAtPosition(i).toString();
+                movie.setGenre(Genre.valueOf(genre));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
         sbDuration = findViewById(R.id.sbDuration);
+        sbDuration.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                movie.setDuration(i);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
         btnRelease = findViewById(R.id.btnRelease);
+        btnRelease.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar c = Calendar.getInstance();
+                int year = c.get(Calendar.YEAR);
+                int month = c.get(Calendar.MONTH);
+                int day = c.get(Calendar.DAY_OF_MONTH);
+                DatePickerDialog datePickerDialog = new DatePickerDialog(MainActivity.this,
+                        new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                        c.set(Calendar.YEAR, year);
+                        c.set(Calendar.MONTH, month);
+                        c.set(Calendar.DAY_OF_MONTH, day);
+                        Date release =c.getTime();
+                        SimpleDateFormat dateFormat = new SimpleDateFormat("MMM dd YYYY");
+                        movie.setRelease(release);
+                        btnRelease.setText(dateFormat.format(release));
+                    }}, year, month, day);
+                datePickerDialog.show();
+            }
+        });
     }
 
     @Override
